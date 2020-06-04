37d47ced901f6d940456f25bdc853dcbd3fb0a97
wilson <wilson.wang@ucloud.cn>
2020-06-04 08:19:13 +0800

    🤔 update

--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import { themeConfig } from './theme';
-import Badge from './components/Badge';
+import Calendar from './components/Calendar';
 
 const H = () => (
   <ThemeProvider theme={themeConfig}>
-    <Badge dot style={{ marginRight: 20 }}>
-      测试一下
-    </Badge>
-    <Badge count={0} style={{ marginRight: 20 }}>
-      测试一下
-    </Badge>
-    <Badge count={12} style={{ marginRight: 20 }}>
-      测试一下
-    </Badge>
-    <Badge count={12} color="blue">
-      测试一下
-    </Badge>
+    <Calendar />
   </ThemeProvider>
 );
 
